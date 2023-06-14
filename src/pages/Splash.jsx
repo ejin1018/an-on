fe1b9de005c91974c99a33730677d6af5ac3bc38
ejin1018 <amy1018@naver.com>
2023-06-14 16:32:00 +0900

    230614 splash 화면 모션 추가

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,16 +1,30 @@
-import {React,useRef} from "react";
+import {React,useRef,useState,useEffect} from "react";
 import {Link} from "react-router-dom"
 
 function Splash(){
-  let refLogo = useRef();
+  let spLogo = useRef();
+  let [loginBlock,setLoginBlock] = useState(false);
+
+  useEffect(()=>{
+    spLogo.current.className = 'splash-logo splash-logo-on'
+    console.log(spLogo.current.className)
+    setTimeout(()=>{
+      setLoginBlock(true)
+    },2300)
+  },[])
+
   return(
     <div className="splash-wrap">
       <div className="splash-container">
-        <img ref={refLogo} className="splash-logo" src="/images/fullLogo.png" alt="AnOn Logo" />
-        <div className="login-box">
-          <Link to={'/login'} className="login-btn login-email">이메일로 시작하기</Link>
-          <Link to={'/home'} className="not-login">회원가입 없이 먼저 둘러보기</Link>
-        </div>
+        <img ref={spLogo} className="splash-logo" src="/images/fullLogo.png" alt="AnOn Logo" />
+        {loginBlock?
+          <div className="login-box">
+            <Link to={'/login'} className="login-btn login-email">이메일로 시작하기</Link>
+            <Link to={'/home'} className="not-login">회원가입 없이 먼저 둘러보기</Link>
+          </div>
+        :
+        ``
+        }
       </div>
     </div>
     )
