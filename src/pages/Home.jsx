b8942673727ec79f1a61c14eb148c65752b27a0d
ejin1018 <amy1018@naver.com>
2023-06-23 19:50:40 +0900

    230623 offer 영역

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,10 +13,9 @@ export default function Home(){
     <div>
       <BotNav />
       <section className="intro">
-        <h2 className="hidden">더 이상 집에서 일어나는 문제에 스트레스 받지 마세요!</h2>
         <div className="intro-inner">
           <p className="intro-sub-title">원인 모를 고장, 미루게 되는 교체 ...</p>
-          <p className="intro-title">더 이상 집에서 일어나는 문제에 스트레스 받지 마세요!</p>
+          <h2 className="title">더 이상 집에서 일어나는 문제에 스트레스 받지 마세요!</h2>
         </div>
         <Swiper
           slidesPerView={1.5}
@@ -54,6 +53,42 @@ export default function Home(){
             </div>
           </SwiperSlide>
         </Swiper>
+      </section>
+      <section className="offer">
+        <div className="offer-inner">
+          <h2 className="section-title">다가오는 여름, 벌레와의 전쟁</h2>
+          <h3 className="section-subtitle">안온이 추천드리는 <b>해충 방역 업체</b></h3>
+          <Swiper
+            slidesPerView={1.5}
+            spaceBetween={18}
+            className="offer-slide"
+          >
+            <SwiperSlide>
+              <div className="offer-company">
+                <p className="offer-company-name">다자바</p>
+                <p className="offer-company-info">서울,경기 | 최소 견적 13만원 ~</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="offer-company">
+                <p className="offer-company-name">다자바</p>
+                <p className="offer-company-info">서울,경기 | 최소 견적 13만원 ~</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="offer-company">
+                <p className="offer-company-name">다자바</p>
+                <p className="offer-company-info">서울,경기 | 최소 견적 13만원 ~</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="offer-company">
+                <p className="offer-company-name">다자바</p>
+                <p className="offer-company-info">서울,경기 | 최소 견적 13만원 ~</p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </section>
     </div>
   )
