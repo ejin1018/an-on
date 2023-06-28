975309f97a513cd2345c90869651543c5fbc237d
ejin1018 <amy1018@naver.com>
2023-06-29 00:06:36 +0900

    230629 로그인 화면

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login(){
   return(
-    <>
-      <p>login</p>
-    </>
+    <div className="login">
+      <div className="login-inner">
+        <Link to={'/'} className="splash-logo">
+          <img src="/images/fullLogo.png" alt="" />
+        </Link>
+        <form action="post">
+          <input type="email" placeholder="이메일" />
+          <input type="password" placeholder="비밀번호" />
+          <button type="submit">로그인</button>
+        </form>
+      </div>
+    </div>
   )
 }
