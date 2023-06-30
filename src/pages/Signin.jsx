8c5ccb7aab1df842e3d045da35663ab7f38cddd9
ejin1018 <amy1018@naver.com>
2023-06-30 23:18:08 +0900

    230630 회원가입 localStorage로 관리

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,29 +1,62 @@
-import React from "react";
+import React,{ useState } from "react";
 import { Link } from "react-router-dom";
 
+
 export default function Signin(){
+  const [userMail,setUserMail] = useState("");
+  const [userPw,setUserPw] = useState("");
+  const [userRepw,setUserRepw] = useState("");
+  const [userNick,setUserNick] = useState("");
+  const signEmFn = (e)=>{
+    setUserMail(e.target.value)
+  }
+  const signPwFn = (e)=>{
+    setUserPw(e.target.value)
+  }
+  const signRePwFn = (e)=>{
+    setUserRepw(e.target.value)
+  }
+  const signNickFn = (e)=>{
+    setUserNick(e.target.value)
+  }
+  
+  const SigninFn = (e)=>{
+    e.preventDefault();
+    localStorage.setItem('email',userMail);
+    localStorage.setItem('pw',userPw);
+    localStorage.setItem('nick',userNick);
+  }
+
   return(
     <div className="signin">
       <div className="signin-inner">
         <Link to={'/'} className="splash-logo">
           <img src="/images/fullLogo.png" alt="" />
         </Link>
-        <form action="post" className="logNsign-form">
+        <form action="post" className="logNsign-form" onSubmit={SigninFn}>
           <input 
             type="email" 
             placeholder="이메일" 
+            value={userMail}
+            onChange={signEmFn}
           />
           <input 
             type="password" 
             placeholder="비밀번호"
+            value={userPw}
+            onChange={signPwFn}
           />
           <input 
             type="password" 
             placeholder="비밀번호 확인"
+            value={userRepw}
+            onChange={signRePwFn}
           />
           <input 
             type="text" 
             placeholder="별명"
+            value={userNick}
+            onChange={signNickFn}
           />
           <button type="submit">회원가입</button>
         </form>
