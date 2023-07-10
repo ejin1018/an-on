ba2ce6cae25243f91cc4f9bdcda3306a627ae676
ejin1018 <amy1018@naver.com>
2023-07-10 21:23:54 +0900

    230710 마이페이지 스타일

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -4,6 +4,8 @@ import Login from "./Login";
 import { logoutAction } from "../actions";
 import { useNavigate,Link } from "react-router-dom";
 import BotNav from "../components/BotNav"
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 export default function Mypage(){
   const user = useSelector(state => state);
@@ -24,42 +26,51 @@ export default function Mypage(){
       : (
       <div className="mypage">
         <BotNav />
-        <div className="mypage-inner">
-          <section className="mypage-profile">
-            <h2 className="mypage-profile-name">{user.nickname} 님의 마이페이지</h2>
-            <button onClick={logoutFn}>로그아웃</button>
-          </section>
-          <section className="mypage-calendar">
-            <h3 className="mypage-title">안온 캘린더</h3>
-          </section>
-          <section className="mypage-myanon">
-            <h3 className="mypage-title">나의 안온</h3>
-            <ul>
-              <li>
-                <Link to={'/'}>고장 접수 확인</Link>
-              </li>
-              <li>
-                <Link to={'/'}>요금 정산 확인</Link>
-              </li>
-              <li>
-                <Link to={'/'}>이사 예약 확인</Link>
-              </li>
-            </ul>
-          </section>
-          <section className="mypage-notice">
-            <ul>
-              <li>
-                <Link to={'/'}>공지사항</Link>
-              </li>
-              <li>
-                <Link to={'/'}>자주 묻는 질문</Link>
-              </li>
-              <li>
-                <Link to={'/'}>약관 및 정책</Link>
-              </li>
-            </ul>
-          </section>
-        </div>
+        <section className="mypage-profile">
+          <h2 className="mypage-profile-name">{user.nickname} 님의 마이페이지</h2>
+          <button onClick={logoutFn}>로그아웃</button>
+        </section>
+        <section className="mypage-calendar">
+          <h3 className="mypage-title">안온 캘린더</h3>
+          <FullCalendar 
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            locale={'ko'}
+            events={[
+              {title: '관리비 정산', date: '2023-07-10'},
+              {title: '입주 청소', date: '2023-07-13'},
+            ]}
+          />
+        </section>
+        <section className="mypage-myanon">
+          <h3 className="mypage-title">나의 안온</h3>
+          <ul>
+            <li>
+              <Link to={'/'}>고장 접수 확인</Link>
+            </li>
+            <li>
+              <Link to={'/'}>요금 정산 확인</Link>
+            </li>
+            <li>
+              <Link to={'/'}>이사 예약 확인</Link>
+            </li>
+          </ul>
+        </section>
+        <section className="mypage-notice">
+          <ul>
+            <li>
+              <Link to={'/'}>공지사항</Link>
+            </li>
+            <li>
+              <Link to={'/'}>자주 묻는 질문</Link>
+            </li>
+            <li>
+              <Link to={'/'}>약관 및 정책</Link>
+            </li>
+          </ul>
+        </section>
       </div>
       )}
     </>
