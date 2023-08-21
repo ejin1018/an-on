30bdbaa6a91497a85082d8d25f98a9b9d364152e
ejin1018 <amy1018@naver.com>
2023-08-21 20:02:07 +0900

    230821 이삿날 첫 섹션 마무리

--- src/pages/Moving.jsx
+++ src/pages/Moving.jsx
@@ -1,8 +1,9 @@
 import { useEffect,useState } from "react";
 import { useSelector } from "react-redux";
-import BotNav from "../components/BotNav";
 import axios from "axios";
 import { API_URL } from "../config/url"
+import BotNav from "../components/BotNav";
+import Login from "./Login";
 
 export default function Moving(){
   const user = useSelector(state => state);
@@ -67,90 +68,108 @@ export default function Moving(){
       day:Number(e.target.value)
     })
   }
-
   const movingdaySubmitFn = (e)=>{
     e.preventDefault();
+    setMovingForm(false);
     axios.post(`${API_URL}/anonuser/${user.id}`,{
-      movingday:`${form.year}년 ${form.month}월 ${form.day} 일`
+      movingday:`${form.year}년 ${form.month}월 ${e.target[2].value} 일`
     }).then((result)=>{
       console.log('이삿날 등록 성공',result)
     }).catch((err)=>{
       console.log('이삿날 등록 실패',err)
     })
   }
+
   return(
-    <div className="moving">
-      <BotNav />
-        {/* 회원 개인 정보에 movingDay 데이터가 비어있으면 my-moving-day 팝업 */}
-        {/* <p> 오늘 getDate 랑 계산해서 d- 남았습니다 보여주기</p> */}
-        <section className="my-moving-day">
-          <h3 className="section-title">나의 이삿날은</h3>
-          {userInfo.movingday == null?
-            <div className="my-moving-day-text">
-              <p>아직 등록되지 않았습니다</p>
-              <button onClick={()=>{setMovingForm(true)}}>등록하기</button>
+    <>
+    {
+      user.email === null?
+      <Login />
+      :
+      <div className="moving">
+        <BotNav />
+          {/* 회원 개인 정보에 movingDay 데이터가 비어있으면 my-moving-day 팝업 */}
+          {/* <p> 오늘 getDate 랑 계산해서 d- 남았습니다 보여주기</p> */}
+          <section className="my-moving">
+            <div className="my-moving-day">
+              <h3 className="section-title">나의 이삿날은</h3>
+              <div className="my-moving-day-text">
+                {userInfo.movingday == null?
+                  <p>아직 등록되지 않았습니다</p>
+                :
+                  <p>{userInfo.movingday}</p>
+                }
+                <button 
+                  className={movingForm?`btn-moddifing btn-just`:`btn-just`}
+                  onClick={()=>{!movingForm?setMovingForm(true):setMovingForm(false)}} />
+              </div>
             </div>
-          :
-            <div className="my-moving-day-text">
-              <p>{userInfo.movingday}</p>
-              <button onClick={()=>{setMovingForm(true)}}>변경하기</button>
-            </div>
-          }
-          {movingForm &&
-            <form method="get" onSubmit={movingdaySubmitFn}>
-              <select 
-                name="moving-year" 
-                id="moving-year-list"
-                onChange={changeYearFn}
+            {movingForm &&
+              <form
+                method="get" 
+                className="moving-date-form"
+                onSubmit={movingdaySubmitFn}
               >
-                {yearList.map((value)=>{
-                  return(
-                    <option value={value} key={value}>{value}년</option>
-                  )
-                })}
-              </select>
-              <select 
-                name="moving-month" 
-                id="moving-month-list"
-                onChange={changeMonthFn}
-                defaultValue={monthList[today.getMonth()].eng}
-              >
-                {monthList.map((value)=>{
-                  return(
-                    <option value={value.eng} key={value.eng}>{value.num}월</option>
-                  )
-                })}
-              </select>
-              <select 
-                name="moving-day" 
-                id="moving-day-list"
-                onChange={changeDayFn}
-                defaultValue={today.getDate()}
-              >
-                {dayList.map((value)=>{
-                  return(
-                    <option value={value} key={value}>{value}일</option>
-                  )
-                })}
-              </select>
-              <button type="submit">등록하기</button>
+                <select 
+                  name="moving-year" 
+                  id="moving-year-list"
+                  onChange={changeYearFn}
+                >
+                  {yearList.map((value)=>{
+                    return(
+                      <option value={value} key={value}>{value}년</option>
+                    )
+                  })}
+                </select>
+                <select 
+                  name="moving-month" 
+                  id="moving-month-list"
+                  onChange={changeMonthFn}
+                  defaultValue={monthList[today.getMonth()].eng}
+                >
+                  {monthList.map((value)=>{
+                    return(
+                      <option value={value.eng} key={value.eng}>{value.num}월</option>
+                    )
+                  })}
+                </select>
+                <select 
+                  name="moving-day" 
+                  id="moving-day-list"
+                  onChange={changeDayFn}
+                  defaultValue={today.getDate()}
+                >
+                  {dayList.map((value)=>{
+                    return(
+                      <option value={value} key={value}>{value}일</option>
+                    )
+                  })}
+                </select>
+                {
+                  userInfo.movingday == null?
+                  <button type="submit">등록하기</button>
+                  :
+                  <button type="submit">변경하기</button>
+                }
+              </form>
+            }
+          </section>
+          <section className="moving-check">
+            <h3>요금 납부</h3>
+            <form action="" method="get">
+              <input type="text" />
+              <button type="submit">등록</button>
             </form>
-          }
-        </section>
-        <section className="moving-check">
-          <h3>요금 납부</h3>
-          <form action="" method="get">
-            <input type="text" />
-            <button type="submit">등록</button>
-          </form>
-          <ul className="moving-check-list">
-            <li>
-              <label>
-                <input type="checkbox" name="" id="" /> 주차비
-              </label>
-            </li>
-          </ul>
-        </section>
-    </div>
+            <ul className="moving-check-list">
+              <li>
+                <label>
+                  <input type="checkbox" name="" id="" /> 주차비
+                </label>
+              </li>
+            </ul>
+          </section>
+      </div>
+    }
+    </>
   )
 }
